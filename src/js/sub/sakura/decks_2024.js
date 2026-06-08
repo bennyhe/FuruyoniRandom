31eca6459f3d4ffa74de89cc500b6bcec95d9322
bennyhe <hesiyuan009@gmail.com>
2026-06-08 18:49:26 +0800

    refactor: config update

--- src/js/sub/sakura/decks_2024.js
+++ src/js/sub/sakura/decks_2024.js
@@ -973,6 +973,20 @@ export default [
     'decklink': '//sdrice.name/random/?ver=S10-2&lang=cn&isShow=1&girls=12+0,24+0&girlscard1=0,1,5+1&girlscard2=0,2,3,6+2,3'
   },
   {
+    'date': '2026-06-07',
+    'author': '可爱的北岸',
+    'name': '伞锤大魔像',
+    'desc': '双黄cos大魔像。纯开伞标准构筑，非常适配新版无常其心，无常大地碎的配合非常强，粘在脸上很多pick对这玩意没啥办法。',
+    'decklink': '//sdrice.name/random/?ver=res1&lang=cn&isShow=1&girls=5+0,7+0&girlscard1=0,2,3,5+1,2&girlscard2=0,1,2+2'
+  },
+  {
+    'date': '2026-06-07',
+    'author': '可爱的北岸',
+    'name': '铳骑走A',
+    'desc': '凶弹 零时 狂想 ae ob 黑盒 随便选，整体考虑到油量的花费抉择we和stc。再演铳的后退能力大幅度增强，麦林手感相当之好。满集中的情况下，铳本家2距离脱3后退4后跳5麦林6可以直接衔接到速射，所以零时是可以进连段的，采用比新幕会多一些，相应领域就不厉害了。也因此在后退的过程中可以把中距刀甩出来。这里配合3-7的bs更是随便退，一边退一边甩ae难以想象有多爽。',
+    'decklink': 'https://sdrice.name/random/?ver=res1&lang=cn&isShow=1&girls=2+0,10+0&girlscard1=0,1,2,4,5+0,1&girlscard2=0,1+0'
+  },
+  {
     'date': '2026-06-08',
     'author': '横浜MYGO！',
     'name': '新时代铳镰',
@@ -992,5 +1006,19 @@ export default [
     'name': '毒爪',
     'desc': '新的缠毒给了1/2人很好的斩杀手段，爪比较适配这一套思路，基本就是前期站2用兽爪压血，无脑跑风轴，斩杀轮缠毒开启后风轴2刀22+霞毒一刀22配合4手（风雷爪此时也是22）',
     'decklink': '//sdrice.name/random/?ver=res1&lang=cn&isShow=1&girls=8+0,11+0&girlscard1=0,2,3,4,5+1,2&girlscard2=0,1+2'
+  },
+  {
+    'date': '2026-06-08',
+    'author': '秦雨',
+    'name': '雪橇车',
+    'desc': '两个增加了攻击距离的女神一拍即合，成为新时代的阴间pick。主思路是7距开始、11ae、21骑动、雪刃、寒雪、前进1或者后退1、11ae、冻僵，打完5/7距对面2甲3冻，下回合可以33感谢炮打血，这一套打中就赢了一半了。上述思路只需要2王牌位+4通常牌，其他卡位非常灵活多变，比如针对一些减伤的对应，带霜之棘，就有32/42骑动和44感谢炮；针对爪这种近距离，带倒车退；针对橇、伞这张单卡攻击收益高的，带ts；针对刀这种大切，带变身下气，就算防你的naga，也能变抽牌斩杀。',
+    'decklink': '//sdrice.name/random/?ver=res1&lang=cn&isShow=1&girls=10+0,14+0&girlscard1=0,1,3+0&girlscard2=0,1,2,5+0,2'
+  },
+  {
+    'date': '2026-06-08',
+    'author': '可爱的北岸',
+    'name': '伞撬闭',
+    'desc': '往后走的时候，预演拒引总是能打出装，这时候冻一冻对手不好前进。别的没了。',
+    'decklink': '//sdrice.name/random/?ver=res1&lang=cn&isShow=1&girls=5+0,14+0&girlscard1=0,1,2,4+0,3&girlscard2=0,2,5+2'
   }
 ]
