3453defe53c159ae196190eafb88c9dc71fe6e6f
bennyhe <hesiyuan009@gmail.com>
2026-08-14 17:32:56 +0800

    refactor: config update

--- src/js/roll-other-data.js
+++ src/js/roll-other-data.js
@@ -5,16 +5,16 @@ import {
 } from './sub/sakura/sakura-with-card'
 
 import sakuraCMData from './sub/sakura/sakura-cmdata.js'
-import decks2022 from './sub/sakura/decks/2022.js'
-import decks2024 from './sub/sakura/decks/2024.js'
-import decks2026 from './sub/sakura/decks/2026.js'
+// import decks2022 from './sub/sakura/decks/2022.js'
+// import decks2024 from './sub/sakura/decks/2024.js'
+import decks2026 from './sub/sakura/decks/2026_re.js'
 import sakuraChangeCardsData from './sub/sakura/sakura-changedata.js'
 
 window.sakuraOtherData = {
   sakuraWithData,
   sakuraStoryData: [],
   sakuraOtherData,
-  sakuraPlayerDeckData: [...decks2022, ...decks2024, ...decks2026].reverse(),
+  sakuraPlayerDeckData: [...decks2026].reverse(),
   sakuraPlayerDeckDataAnySeason: '再演S1',
   sakuraCMData,
   sakuraChangeCardsData
